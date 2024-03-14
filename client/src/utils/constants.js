dd51f6b1bad2b2ecf45e8c90aaa9b08dfcf8ab95
pnzenang <pnzenang@gmail.com>
2024-03-14 04:52:50 -0400

    One more commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -7,7 +7,7 @@ export const deathAnnouncement = [
 ];
 
 export const comfortMessage = [
-  `The SAGI Community extends its deepest sympathies to the association and to the bereaver family. May you find courage and strength to carry your late loved one legacy on.`,
-  `We are holding the association and the bereaver family close in our thoughts and prayer as you navigate through this painful time. late loved one presence will always be felt, and your late loved one love will continue to surround you.`,
-  `We can’t begin to understand the pain the association and the bereaver family are feeling, but please rest assured that your late loved one kindness and love will forever be remembered.`,
+  `The SAGI Community extends its deepest sympathies to the association and to the bereaved family. May you find courage and strength to carry your late loved one legacy on.`,
+  `We are holding the association and the bereaved family close in our thoughts and prayer as you navigate through this painful time. late loved one presence will always be felt, and your late loved one love will continue to surround you.`,
+  `We can’t begin to understand the pain the association and the bereaved family are feeling, but please rest assured that your late loved one kindness and love will forever be remembered.`,
 ];
