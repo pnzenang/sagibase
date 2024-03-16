e39fe5ff374de0b064aff331bbcb1e1cb0138e9e
pnzenang <pnzenang@gmail.com>
2024-03-16 05:04:06 -0400

    3.16.2.commit

--- client/src/components/MonthAtGlance.jsx
+++ client/src/components/MonthAtGlance.jsx
@@ -6,7 +6,7 @@ const MonthAtGlance = () => {
       id='timetable'
       className='relative bg-base-200   pb-4  rounded-xl lg:py-20   '
     >
-      <h2 className='text-base-content text-center px-8  py-12 text-2xl font-bold sm:text-4xl md:text-[40px]'>
+      <h2 className='text-base-content text-center px-8  py-4 text-2xl font-bold sm:text-4xl md:text-[40px]'>
         At SAGI, the month is divided into 3 big determining moments
       </h2>
       <div className='container align-element'>
