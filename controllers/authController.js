52354c61b0615e932a530528b741d441fad6cdec
pnzenang <pnzenang@gmail.com>
2024-03-19 03:08:25 -0400

    3.19.2.commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -128,7 +128,7 @@ export const forgotPassword = async (req, res) => {
     const passwordToken = crypto.randomBytes(70).toString('hex');
     //send email
     // const origin = "http://active-solidarity-com.onrender.com";
-    const origin = 'https://sagibase.onrender.com/';
+    const origin = 'https://sagibase.onrender.com';
 
     await sendResetPasswordEmail({
       name: `${user.firstName}  ${user.lastAndMiddleNames}`,
