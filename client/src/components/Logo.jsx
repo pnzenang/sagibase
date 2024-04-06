b8a7127bdb276170b6240d09b31d2f15e983316a
pnzenang <pnzenang@gmail.com>
2024-04-06 10:51:23 -0400

    4.6.1.commit

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <NavLink to='/' className='flex  text-3xl items-center'>
       <img
-        src='https://res.cloudinary.com/dp8tkb7hq/image/upload/v1712362303/sagilogo_rknjjp.svg'
+        src='https://res.cloudinary.com/dp8tkb7hq/image/upload/v1712414233/sagilogo2_c82cpj.svg'
         alt='logo'
       />
     </NavLink>
