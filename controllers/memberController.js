c09db82ddee35598d345f86c7f32377e35454908
pnzenang <pnzenang@gmail.com>
2025-01-08 19:39:17 -0500

    1.8.1.commit

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -54,7 +54,7 @@ export const getAllMembers = async (req, res) => {
 
   // setup pagination
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 300;
   const skip = (page - 1) * limit;
 
   const members = await Member.find(queryObject)
--- controllers/userController.js
+++ controllers/userController.js
@@ -96,7 +96,7 @@ export const adminGetAllMembers = async (req, res) => {
 
   // setup pagination
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 300;
   const skip = (page - 1) * limit;
 
   const adminMembers = await Member.find(queryObject)
